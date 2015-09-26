4f56af243347bf9ed6909a8668b238e6a683d8d8
Homer Strong <homer.strong@gmail.com>
2015-09-26 13:32:07 -0700

    try more noise

--- regressor.py
+++ regressor.py
@@ -7,7 +7,7 @@ class Regressor(BaseEstimator):
         self.gp = GaussianProcess(corr='absolute_exponential')
 
     def fit(self, X, y):
-        y = y + np.random.randn(len(y)) * (1/20)
+        y = y + np.random.randn(len(y)) * (1/10)
         self.gp.fit(X, y)
 
     def predict(self, X):
